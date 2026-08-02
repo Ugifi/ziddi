e19e684e3ab6e9118d815c867c4bbb1ae3e6fe63
Ugifi <79358347+Ugifi@users.noreply.github.com>
2026-08-02 18:31:08 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@
   import Toast from './components/Toast';
   import { DepositModal } from './pages/OtherPages';
   import { WithdrawModal } from './components/Modals';
+import DownloadPage from './pages/DownloadPage';
 
   import HomeScreen from './pages/HomeScreen';
   import GameTypePage from './pages/GameTypePage';
